113d719bf2530fdafb47a0541b6eb3c96f50f933
kevin mijares <kevmdm@gmail.com>
2022-03-08 09:12:36 -0400

    separated functions

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends (React.Component,BlockFrostRequest) {
 
     let api = await window.cardano.nami.enable();
     this.getData(api);
-    this.fetchBlockfrost();
+    this.fetchBlockfrostUnits(this.fetchInit);
   }
 
 
--- src/blockFrostRequest.js
+++ src/blockFrostRequest.js
@@ -6,18 +6,20 @@ const pageWallet = 'addr_test1wrsm2a8hl809ran3paw999vct5mgcwtzajkntzs8thqkses8gf
 const aleWallet = 'addr_test1qrc6fdexcnkmt9xau0e7yv9yaxyscd06klcme4nvj9c8d2gw9vs4u5282ymd67uf666yd6qldlmhn703qgt6lxcwk0qs7mps3y'
 const key = process.env.REACT_APP_API_KEY;
 
+
+
 class BlockFrostRequest extends React.Component {
     
-
-    async fetchBlockfrost() {
-
-        var fetchInit = {
-            method: 'GET',
-            headers: {
-                project_id: key
-            }
+    fetchInit = {
+        method: 'GET',
+        headers: {
+            project_id: key
         }
+    }
 
+    async fetchBlockfrostUnits(fetchInit) {
+
+        //esta funcion se debe mejorar para imprimir todos los units 
         let jsonRes = await (await (fetch('https://cardano-testnet.blockfrost.io/api/v0/addresses/' + aleWallet, fetchInit))).json();
         //console.log('imprimiendo jsonRes')
         //console.log(jsonRes);
@@ -26,6 +28,11 @@ class BlockFrostRequest extends React.Component {
         let unitMetadataJson = await (await (fetch('https://cardano-testnet.blockfrost.io/api/v0/assets/' + unit, fetchInit))).json();
         console.log(unitMetadataJson);
 
+
+    }
+
+    async fetchWalletUtxo(fetchInit){
+        
         let walletUtxo = await (await (fetch('https://cardano-testnet.blockfrost.io/api/v0/addresses/' + pageWallet + '/utxos', fetchInit))).json();
         console.log('imprimiendo utxo');
         console.log(walletUtxo);
