5352535f81919fad0302d7aeb449dd15d1bb1069
kevin mijares <kevmdm@gmail.com>
2022-03-08 09:04:46 -0400

    BlockfrostRequest on its own file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import logo from './logo.svg';
 import * as wasm from "https://cdn.jsdelivr.net/npm/@emurgo/cardano-serialization-lib-asmjs@10.0.4/cardano_serialization_lib.js";
+import BlockFrostRequest from './blockFrostRequest.js';
 
 function hexToBytes(hex) {
   for (var bytes = [], c = 0; c < hex.length; c += 2)
@@ -9,9 +10,6 @@ function hexToBytes(hex) {
   return bytes;
 }
 
-const pageWallet = 'addr_test1wrsm2a8hl809ran3paw999vct5mgcwtzajkntzs8thqkses8gfqln';
-const aleWallet = 'addr_test1qrc6fdexcnkmt9xau0e7yv9yaxyscd06klcme4nvj9c8d2gw9vs4u5282ymd67uf666yd6qldlmhn703qgt6lxcwk0qs7mps3y'
-const key = process.env.REACT_APP_API_KEY;
 
 class UserInfo extends React.Component {
 
@@ -26,7 +24,7 @@ class UserInfo extends React.Component {
     )
   }
 }
-class App extends React.Component {
+class App extends (React.Component,BlockFrostRequest) {
 
   constructor(props) {
     super(props);
@@ -42,36 +40,6 @@ class App extends React.Component {
     let api = await window.cardano.nami.enable();
     this.getData(api);
     this.fetchBlockfrost();
-
-  }
-
-  async fetchBlockfrost() {
-
-    var fetchInit = {
-      method: 'GET',
-      headers: {
-        project_id: key
-      }
-    }
-    //funciona 
-    // fetch('https://cardano-testnet.blockfrost.io/api/v0/addresses/'+ pageWallet , fetchInit)
-    //   .then((res) => res.json())
-    //   .then((json) => {
-    //     console.log(json.amount[1].unit)
-    //   });
-    //nuevo codigo 
-    // este funciona mejor tengo acceso a las variables
-    let jsonRes = await (await (fetch('https://cardano-testnet.blockfrost.io/api/v0/addresses/' + aleWallet, fetchInit))).json();
-    //console.log('imprimiendo jsonRes')
-    //console.log(jsonRes);
-    let unit = jsonRes.amount[2].unit;
-    console.log(unit);
-    let unitMetadataJson = await (await (fetch('https://cardano-testnet.blockfrost.io/api/v0/assets/' + unit, fetchInit))).json();
-    console.log(unitMetadataJson);
-
-    let walletUtxo = await (await (fetch('https://cardano-testnet.blockfrost.io/api/v0/addresses/'+pageWallet+'/utxos', fetchInit))).json();
-    console.log('imprimiendo utxo');
-    console.log(walletUtxo);
   }
 
 
---
+++ src/blockFrostRequest.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+
+
+const pageWallet = 'addr_test1wrsm2a8hl809ran3paw999vct5mgcwtzajkntzs8thqkses8gfqln';
+const aleWallet = 'addr_test1qrc6fdexcnkmt9xau0e7yv9yaxyscd06klcme4nvj9c8d2gw9vs4u5282ymd67uf666yd6qldlmhn703qgt6lxcwk0qs7mps3y'
+const key = process.env.REACT_APP_API_KEY;
+
+class BlockFrostRequest extends React.Component {
+    
+
+    async fetchBlockfrost() {
+
+        var fetchInit = {
+            method: 'GET',
+            headers: {
+                project_id: key
+            }
+        }
+
+        let jsonRes = await (await (fetch('https://cardano-testnet.blockfrost.io/api/v0/addresses/' + aleWallet, fetchInit))).json();
+        //console.log('imprimiendo jsonRes')
+        //console.log(jsonRes);
+        let unit = jsonRes.amount[2].unit;
+        console.log(unit);
+        let unitMetadataJson = await (await (fetch('https://cardano-testnet.blockfrost.io/api/v0/assets/' + unit, fetchInit))).json();
+        console.log(unitMetadataJson);
+
+        let walletUtxo = await (await (fetch('https://cardano-testnet.blockfrost.io/api/v0/addresses/' + pageWallet + '/utxos', fetchInit))).json();
+        console.log('imprimiendo utxo');
+        console.log(walletUtxo);
+    }
+}
+
+export default BlockFrostRequest
